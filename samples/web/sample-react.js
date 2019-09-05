9d690d9200675e4ccacded3eded41e44c711fd58
Klem <kaelhem@gmail.com>
2019-09-05 15:06:57 +0200

    updated samples

--- samples/web/sample-react.js
+++ samples/web/sample-react.js
@@ -5,16 +5,23 @@ import { saveAs } from 'file-saver'
 export default () => {
   const [content, setContent] = useState('')
   
-  const createStl = () => {
-    const qr3dAscii = qr3D(content)
+  const createAscii = () => {
+    const qr3dAscii = qr3D(content, { binary: false }).data
     const blob = new Blob([qr3dAscii], {type: 'text/plain;charset=utf-8'})
-    saveAs(blob, 'sample.stl')
+    saveAs(blob, 'sample-ascii.stl')
+  }
+
+  const createBinary = () => {
+    const qr3dBin = qr3D(content).data
+    const blob = new Blob([qr3dBin], {type: 'text/plain;charset=utf-8'})
+    saveAs(blob, 'sample-binary.stl')
   }
   return (
     <Fragment>
       <span>String to encode in QRCode:</span>
       <input value={content} onChange={ (e) => setContent(e.target.value) } />
-      <button disabled={content === ''} onClick={ createStl }>create .stl</button>
+      <button disabled={content === ''} onClick={ createAscii }>create ascii .stl</button>
+      <button disabled={content === ''} onClick={ createAscii }>create binary .stl</button>
     </Fragment>
   )
 }
